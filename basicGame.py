e1cf42b0f410795fb2421f2f5afe770f20cfcda2
ashutoshpaliwal26 <ashutoshpaliwal26@gmail.com>
2023-06-02 12:22:35 +0530

    Update 0.1

--- basicGame.py
+++ basicGame.py
@@ -12,27 +12,28 @@ black = 0,0,0
 red = 255,0,0
 
 # variable for Player 
-x=20
+player_x = 60
+player_y = 40
 
 #Clock For Game
 clock = pygame.time.Clock()
 
 running  = True
 
-def keypressFuntion():
-    if event.type == pygame.K_UP:
-        for i in range(0,600):
-            player = x+i
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        # Giving Keyboard responce to our Game. 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT :
+                player_x = player_x + 20
+        
     screen.fill(white) #To giving background to our screen
 
     #Render Your Game Here
-
+    pygame.draw.rect(screen, black,[player_x, player_y,20,20])
     pygame.display.update()
 
     clock.tick(60) # limits FPS to 60
