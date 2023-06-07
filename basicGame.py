ceebddcebdfed33d0eec003775d59292b61aa5bb
ashutoshpaliwal26 <ashutoshpaliwal26@gmail.com>
2023-06-07 10:20:01 +0200

    Update 0.5

--- basicGame.py
+++ basicGame.py
@@ -10,22 +10,54 @@ screen_height = 800
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
 
+# Function for Text Rendering on screen
+def RenderText(text, color, x, y, fsize):
+    Font = pygame.font.SysFont(None, fsize)
+    screen_text = Font.render(text, True, color)
+    screen.blit(screen_text, (x, y))
+
 # Giving Title To our Game.
 Title = pygame.display.set_caption("Snake PY")
 
+def welcome():
+    exit_game = False
+    green = 2,99,12
+    white = 255,255,255
+    bg = 242, 85, 68
+    black = 0,0,0
+    screen.fill(bg)
+    RenderText("Snake Py", white, 130, 250, 160)
+    RenderText("Press ! Space to Play", black, 200, 350, 50)
+    while not exit_game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_game = True
+
+            if event.type == pygame.KEYDOWN:
+                if (event.key == pygame.K_SPACE):
+                    gameloop()
+
+        pygame.display.update()
+        clock.tick(60)
+
 def gameloop():
+
+        
     # Creating Colour Variable 
     white = 255,255,255
     black = 0,0,0
     red = 255,0,0
+    green = 2,99,12
+    yellow = 230, 250,0
     blue = 0,0,255
+    bg = 242, 85, 68
 
     # variable for Snake 
     player_x = 50
     player_y = 95
     velocity_x = 0
     velocity_y = 0
-    inti_velocity = 5
+    inti_velocity = 3
 
     # Variables for food 
     food_x = random.randint(100,700)
@@ -34,11 +66,6 @@ def gameloop():
 
     running  = True
 
-    # Function for Text Rendering on screen
-    def RenderText(text, color, x, y, fsize):
-        Font = pygame.font.SysFont(None, fsize)
-        screen_text = Font.render(text, True, color)
-        screen.blit(screen_text, (x, y))
 
 
     #Snake Length Variables 
@@ -51,12 +78,18 @@ def gameloop():
     def plot(Screen, color, coordinate_list , size):
         for x, y in coordinate_list:
             pygame.draw.rect(Screen, color,[x, y, size, size])
-        pass
         
 
+    with open("hiscore.txt", "r") as f:
+        hiscore = f.read()   
+
     while running:
+
         if GameOver:
-            screen.fill(white)
+            with open("hiscore.txt", "w") as f:
+                f.write(str(hiscore))
+
+            screen.fill(bg)
             RenderText("SNAKE PY", blue, 100, 200, 150)
             RenderText("Game Over", red, screen_width/4, 600, 100)
             RenderText("Press Enter ! To Continue", black ,250, 750, 30)
@@ -68,6 +101,10 @@ def gameloop():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RETURN:
                         gameloop()
+                
+
+            if score > int(hiscore) :
+                hiscore = score
 
             
 
@@ -118,10 +155,11 @@ def gameloop():
 
             
 
-            screen.fill(white) #To giving background to our screen
+            screen.fill(bg) #To giving background to our screen
             pygame.draw.rect(screen, black, [10, 40, 780,750])
-            pygame.draw.rect(screen, white, [15, 45, 770, 740])
-            RenderText("Score : " + str(score), blue, 30, 15, 20)
+            pygame.draw.rect(screen, bg, [15, 45, 770, 740])
+            RenderText("Score : " + str(score), blue, 10, 15, 20)
+            RenderText("High Score : " + hiscore, blue, 690, 15, 20)
 
             # Creatin Snake's Head
             head = []
@@ -138,6 +176,8 @@ def gameloop():
             if head in snk_list[:-1]:
                 GameOver = True
 
+
+
             #Render Your Game Here
             pygame.draw.rect(screen, red , [food_x, food_y, 20 ,20])
             # pygame.draw.rect(screen, black,[player_x, player_y,20,20])
@@ -148,4 +188,5 @@ def gameloop():
 
     pygame.quit()
     quit()
-gameloop()
+
+welcome()
